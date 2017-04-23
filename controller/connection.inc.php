5d5a73d320dae1b910651edc56971222b95a7421
serieee <senbonzakura_oak@hotmail.com>
2017-04-23 18:03:02 +0700

    test

--- controller/connection.inc.php
+++ controller/connection.inc.php
@@ -5,6 +5,6 @@
  * Date: 27/2/2560
  * Time: 13:45
  */
-$conn = new PDO('mysql:host=localhost; dbname=workshop10', 'root', '');
+$conn = new PDO('mysql:host=localhost; dbname=workshop4', 'root', '');
 ?>
 
