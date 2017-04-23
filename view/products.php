0eca57dcacfa7abc250fec745c8c4fb7ba78b1f1
serieee <senbonzakura_oak@hotmail.com>
2017-04-23 18:02:27 +0700

    test

--- view/products.php
+++ view/products.php
@@ -6,6 +6,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+
+    <script>
+        function loadProduct() {
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function() { setName(xhr); };
+            var url = "http://localhost/webapp/workshop10/model/db_product.inc.php";
+            xhr.open("POST",url,true);
+            xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+            xhr.send();
+        }
+        function setName(xhr) {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                var r = xhr.responseText;
+
+                if (r.indexOf("ERRNO") >= 0 || r.indexOf("error:") >= 0
+                    || r.length == 0)
+                    throw (r.length == 0 ? "Server error." : r);
+                var product = JSON.parse(r);
+                var table = document.getElementById("table_product");
+                for(var i=0;i<product.length;i++){
+                    var count = i+1;
+                    var tr = document.createElement("tr");
+                    var td1 = document.createElement("td");
+                    td1.innerHTML= count;
+                    tr.appendChild(td1);
+                    var td2 = document.createElement("td");
+                    td2.innerHTML= product[i][0];
+                    tr.appendChild(td2);
+                    var td3 = document.createElement("td");
+                    td3.innerHTML= product[i][1];
+                    tr.appendChild(td3);
+                    var td4 = document.createElement("td");
+                    td4.innerHTML= product[i][2];
+                    tr.appendChild(td4);
+                    var td5 = document.createElement("td");
+                    td5.innerHTML= "<input name='amount[]' type='number' id='amount'/>";
+                    tr.appendChild(td5);
+                    table.appendChild(tr);
+                }
+            }
+        }
+        $(document).ready(function () {
+            $("#amount").keydown(function (e) {
+                var chk = e['key'].charCodeAt(0);
+                console.log(chk);
+
+            });
+        });
+    </script>
 </head>
 <?php
 include ("../view/header.php");
@@ -21,7 +70,7 @@ spl_autoload_register(function ($class_name)
     require_once "../class/".$class_name.".class.php";
 });
 ?>
-<body>
+<body onload="loadProduct()">
 
 <div align="center">
     <h2>เลือกซื้อสินค้า</h2>
@@ -34,7 +83,7 @@ spl_autoload_register(function ($class_name)
         $_SESSION["productS"] = $productString;
         $pro = $proList->getProducts();
         ?>
-        <table class="table table-hover" border='2' style="width: 60%" >
+        <table class="table table-hover" border='2' style="width: 60%" id="table_product">
             <tr style="background-color: #ffff6b">
                 <th>ลำดับ</th>
                 <th>ชื่อสินค้า</th>
@@ -42,27 +91,12 @@ spl_autoload_register(function ($class_name)
                 <th>ราคา</th>
                 <th>จำนวน</th>
             </tr>
-            <?php
-            $j = 1;
-        for ($i = 0;  $i < count($pro);$i++){
-           echo" <tr>
-                <td>".$j."</td>
-                <td>".$pro[$i]->getNamePro()."</td>
-                <td width='15%'>".$pro[$i]->getCodePro()."</td>
-                <td>".$pro[$i]->getPricePro()."</td>
-                <td width='10%'><input type='number' min='0' name='amount[]' id='amonut[]'></td>
-            </tr>
-            ";
-            $j++;
-        }
-            ?>
         </table></p>
 
         <input class="btn btn-success" type="submit" name="buy" id="buy" value="ซื้อสินค้า"/>
     </form>
 </div>
 <?php
-
 include ("../view/footer.php");
 //show_source("products.php");
 ?>
